0da7f652b2ded779e87566c8a487e26c9fcabf04
cimex7 <541904881@qq.com>
2019-03-11 22:20:18 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask
+import os
+import sys
+
+import click
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+#SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///:'
+else:
+    prefix = 'sqlite:////:'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
 
 name = 'cimex'
 movies = [
@@ -16,9 +46,27 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
+user = User(name=name)
+db.session.add(user)
+for m in movies:
+    movie = Movie(title=m['title'], year=m['year'])
+    db.session.add(movie)
+
+db.session.commit()
+click.echo('Done.')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
-	
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies) 
 
 
